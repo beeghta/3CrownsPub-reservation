1540dd6c723b66d275898e608735f565687c2fe1
Beata <bita.zarabie@gmail.com>
2026-09-20 20:54:46 +0330

    db update

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const pool = require("./db");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 
 app.use(cors());
@@ -247,16 +247,13 @@ app.post("/api/reservations", async (req, res) => {
    START SERVER
 ========================= */
 
+app.listen(
+    PORT,
+    () => {
 
-if (process.env.VERCEL !== "1") {
-    app.listen(
-        PORT,
-        () => {
-            console.log(
-                `3Crowns server is running on http://localhost:${PORT}`
-            );
-        }
-    );
-}
+        console.log(
+            `3Crowns server is running on http://localhost:${PORT}`
+        );
 
-module.exports = app;
+    }
+);
