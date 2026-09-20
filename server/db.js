a87198341be00f8d0774c8abae1908ea6aa6ebe3
Beata <bita.zarabie@gmail.com>
2026-09-20 19:38:40 +0330

    Clever Cloud DB

--- server/db.js
+++ server/db.js
@@ -1,12 +1,13 @@
 ﻿const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-    host: "localhost",
+    host: "bogvyoe71lqe2k0ka7a9-mysql.services.clever-cloud.com",
     user: "root",
-    password: "Xurl#TJy*413",
-    database: "3crowns",
+    password: "Xurl3FGRpJknuY9kjK5xbsJrTJy*413",
+    database: "bogvyoe71lqe2k0ka7a9",
     waitForConnections: true,
-    connectionLimit: 10
+    connectionLimit: 2,
+    DB_PORT: 3306
 });
 
 module.exports = pool;
