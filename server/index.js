c34e10577d7538e40df9322b814c5f7726ef21b8
Beata <bita.zarabie@gmail.com>
2026-09-21 17:11:21 +0330

    render db

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const pool = require("./db");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 app.use(cors());
@@ -249,10 +249,11 @@ app.post("/api/reservations", async (req, res) => {
 
 app.listen(
     PORT,
+    "0.0.0.0",
     () => {
 
         console.log(
-            `3Crowns server is running on http://localhost:${PORT}`
+            `3Crowns server is running on port ${PORT}`
         );
 
     }
